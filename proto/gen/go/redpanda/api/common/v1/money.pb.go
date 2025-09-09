f43c204e468c83a53857bc669151a4b6d1a8c1fd
Johannes Brüderl <johannes@redpanda.com>
2025-09-09 21:05:44 +0200

    wip

--- proto/gen/go/redpanda/api/common/v1/money.pb.go
+++ proto/gen/go/redpanda/api/common/v1/money.pb.go
@@ -7,13 +7,12 @@
 package commonv1
 
 import (
-	reflect "reflect"
-	sync "sync"
-	unsafe "unsafe"
-
 	_ "google.golang.org/genproto/googleapis/api/annotations"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
+	reflect "reflect"
+	sync "sync"
+	unsafe "unsafe"
 )
 
 const (
