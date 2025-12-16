f7cc803c9d5aa2b3d67608b7d2e880a94947d0f6
Martin Schneppenheim <23424570+weeco@users.noreply.github.com>
2025-12-16 19:20:04 +0100

    chore: apply code formatting with gci

--- redpanda-otel-exporter/otlp_transform.go
+++ redpanda-otel-exporter/otlp_transform.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math"
 
+	pb "buf.build/gen/go/redpandadata/otel/protocolbuffers/go/redpanda/otel/v1"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/codes"
 	"go.opentelemetry.io/otel/log"
@@ -13,8 +14,6 @@ import (
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	"go.opentelemetry.io/otel/trace"
-
-	pb "buf.build/gen/go/redpandadata/otel/protocolbuffers/go/redpanda/otel/v1"
 )
 
 // int64ToUint64 safely converts an int64 to uint64.
