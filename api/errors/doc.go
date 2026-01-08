db15089970f7561bc69a1aca0d32a8f24fe6e0c0
Andrew Stucki <andrew.stucki@redpanda.com>
2026-01-08 17:20:58 -0500

    Add licenseupdater

--- api/errors/doc.go
+++ api/errors/doc.go
@@ -1,3 +1,12 @@
+// Copyright 2026 Redpanda Data, Inc.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.md
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0
+
 // Package errors The errors package contains several helpers for constructing new connectrpc errors,
 // as well as a helper for writing connect.Errors via HTTP.
 package errors
