0db6469aa3975f5fc1a87bb17a00cdc7f23b961a
Michael Boquard <michael@redpanda.com>
2026-01-30 14:20:15 -0500

    rpsr: Permit use of "Group:" principal for SR ACLs
    Signed-off-by: Michael Boquard <michael@redpanda.com>

--- rpsr/builder.go
+++ rpsr/builder.go
@@ -255,8 +255,8 @@ func (b *ACLBuilder) ValidateCreate() error {
 		return errors.New("invalid deny principals with no denied hosts")
 	}
 	for _, p := range append(b.allowPrincipal, b.denyPrincipal...) {
-		if p != "*" && (!strings.HasPrefix(p, "User:") && !strings.HasPrefix(p, "RedpandaRole:")) {
-			return fmt.Errorf("invalid principal %v: must start with 'User:' or 'RedpandaRole:'", p)
+		if p != "*" && (!strings.HasPrefix(p, "User:") && !strings.HasPrefix(p, "RedpandaRole:") && !strings.HasPrefix(p, "Group:")) {
+			return fmt.Errorf("invalid principal %v: must start with 'User:', 'RedpandaRole:' or 'Group:'", p)
 		}
 	}
 	return nil
